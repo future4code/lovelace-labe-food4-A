02bcefe92cdc2b4091d41954c3bb170a7aa48294
Thayane-Bitencourt <thayanebitencourt.ssa@gmail.com>
2021-09-06 16:18:39 -0300

    criacao do login,cadastro

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import  Router  from './Routes/Router'
+import React, {useState} from "react"
+import Router from "./Routes/Router"
+import theme from './constants/theme'
+import { ThemeProvider } from '@material-ui/core/styles'
+import { BrowserRouter } from 'react-router-dom'
+import Header from "./components/Header"
+
 const App = () => {
-  return(
-    <div> 
-      <h1>LabeFood</h1>
-      <Router />
-    </div>
+  const token = localStorage.getItem("token")
+  const [rightButtonText, setRightButtonText] = useState(token ? "Logout" : "Login")
+
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header rightButtonText={rightButtonText} setRightButtonText={setRightButtonText} />
+        <Router setRightButtonText={setRightButtonText}/>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
-export default App
+export default App;
