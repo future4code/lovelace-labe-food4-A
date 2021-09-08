f34e884b630d48beaf8656ab8c98931bb9db42f7
Elias Sousa <inboxsilva1998@gmail.com>
2021-09-07 22:14:59 -0300

    adicionando estilizacôes e alinhando itens de requisição

--- src/Pages/Resultado/style.js
+++ src/Pages/Resultado/style.js
@@ -22,7 +22,11 @@ export const DescribeContainer = styled.div`
 display: flex;
 flex-direction:column;
 height: 240px;
+width: 92%;
+border-radius:20px;
 background-color: whitesmoke;
+margin-top: 10px;
+
 
 div{
     display: flex;
@@ -33,7 +37,8 @@ h4{
     margin-right:40px;
     margin-bottom:0.5px;
     margin-top:8px;
-    color:red
+    color:red;
+    margin-left: 5px;
     
 }
 
@@ -41,12 +46,14 @@ p{
     margin-right:40px;
     margin-bottom:0.5px;
     margin-top:8px;
+    padding-left: 5px;
     
 }
 
 img{
     width: 100%;
     height:50%;
+    border-radius:20px 20px 0 0;
     
 }
 `;
@@ -56,7 +63,10 @@ export const MenuContainer = styled.div`
 display: flex;
 flex-direction:row;
 height: 112px;
+width: 95%;
 background-color: whitesmoke;
+margin-top:10px;
+border-radius:10px;
 
 div{
     display: flex;
@@ -65,16 +75,47 @@ div{
 }
 
 
+h4{
+    margin-right:40px;
+    margin-bottom:0.5px;
+    margin-top:2px;
+    color:red
+    
+}
+
+
 p{
     margin-right:40px;
     margin-bottom:0.5px;
-    margin-top:8px;
+    margin-top:2px;
     
 }
 
 img{
     width: 30%;
     height:112px;
+    border-radius:20px 0 0 20px  ;
     
 }
+
+
+button{
+    width: 40%;
+    float: right;
+}
+`;
+
+
+
+
+export const Scrow = styled.div`
+overflow:auto;
+height: 280px;
+`;
+
+
+
+export const PutCenter = styled.div`
+display: flex;
+justify-content: center;
 `;
